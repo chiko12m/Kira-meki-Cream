1a9379a3507f8f1f8e1f0fa7a994b5454f11330d
chiko12m <chiko.dct@gmail.com>
2024-06-23 15:38:21 +0900

    202406231538

---
+++ assets/script/main.js
@@ -0,0 +1,31 @@
+// スライド
+$('.voice__group').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+// アコーディオン
+$('.faq__question').click(function(){
+    const answer = $(this).next('.faq__answer-wrap');
+    const arrow = $(this).find('.faq__arrow');
+    answer.slideToggle(300).toggleClass('open');
+    arrow.toggleClass('rotate');
+});
+
+// スクロール
+$('.footer__scroll-to-top').click(function() {
+    $('html, body').animate({scrollTop: 0}, 800);
+    return false;
+});
